bb123e09c66736dd7ad7a1be6afc2dfc0d6cfb22
William6857 <139811180+William6857@users.noreply.github.com>
2026-02-14 15:36:38 -0700

    Add 'angry' behavior

---
+++ commands/behaviors/angry.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'angry',
+    description: 'angry vector',
+    execute(message, args){
+        const cmdFile = require('../../functions/embedFile.js');
+        cmdFile.vectorEmbed(message, "none", "none", "hN7zGan.gif");
+    },
+};
