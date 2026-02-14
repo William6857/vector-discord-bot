84614be3b419d6ef1b7d761139cc454cb9213b74
William6857 <139811180+William6857@users.noreply.github.com>
2026-02-14 15:36:38 -0700

    Change 'thicc' command name to 'dummy' and add 'thicc' arg

--- commands/extra/thicc.js
+++ commands/extra/thicc.js
@@ -1,8 +1,10 @@
 module.exports = {
-	name: 'thicc',
+	name: 'dummy',
 	description: 'Picture of a thicc Vector',
 	execute(message, args) {
       	const cmdFile = require(`../../functions/embedFile.js`);
-		cmdFile.vectorEmbed(message, "~~damN BOI HE ***THICC*** ( ͡° ͜ʖ ͡°)~~", "none", "3x5duTV.png");
+		if(args[0] === "thicc"){
+			cmdFile.vectorEmbed(message, "~~damN BOI HE ***THICC*** ( ͡° ͜ʖ ͡°)~~", "none", "3x5duTV.png");
+		}
 	},
 };
